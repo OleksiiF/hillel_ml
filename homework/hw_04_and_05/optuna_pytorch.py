93ec5733e3dbfb54401977b99a9f77c9850aca29
lex <lex>
2022-09-21 15:14:11 +0300

    shape issue isn't resolved

--- homework/hw_04_and_05/optuna_pytorch.py
+++ homework/hw_04_and_05/optuna_pytorch.py
@@ -35,8 +35,7 @@ class NeuralNetwork(torch_nn.Module):
     def __init__(
             self,
             epochs=27,
-            # dropout=.22,
-            dropout=0,
+            dropout=.22,
             learning_rate=0.03593,
             optimizer='Adagrad',
             activation_func="glu",
@@ -45,10 +44,12 @@ class NeuralNetwork(torch_nn.Module):
             hidden_layer_dimensions=420,
             pca=200
     ):
+        self.test_accuracy = 0.0
         super(NeuralNetwork, self).__init__()
         self.device = device("cuda" if cuda.is_available() else "cpu")
         self.to(self.device)
         # hyperparams
+        self.pca = pca
         self.hidden_layer_dimensions = hidden_layer_dimensions
         self.epochs = epochs
         self.hidden_layers = hidden_layers
@@ -56,7 +57,6 @@ class NeuralNetwork(torch_nn.Module):
         self.dropout = torch_nn.Dropout(dropout)  # prevents overwriting
         self.loss_function = torch_nn.CrossEntropyLoss()
         self.learning_rate = learning_rate
-        self.pca = pca
         self.__optimizer = optimizer
         self.optimizer = getattr(optim, self.__optimizer)(
             self.parameters(),
@@ -76,21 +76,19 @@ class NeuralNetwork(torch_nn.Module):
         self.test_loader = self.get_data_loader(self.test_data)
         self.np_train_data = self.train_data.data.numpy()
         self.np_test_data = self.test_data.data.numpy()
-        train_U, train_S, train_V = pca_lowrank(
+        train_U, self.train_S, train_V = pca_lowrank(
             from_numpy(self.np_train_data).double(),
             niter=self.pca
         )
-        test_U, test_S, test_V = pca_lowrank(
+        test_U, self.test_S, test_V = pca_lowrank(
             from_numpy(self.np_test_data).double(),
             niter=self.pca
         )
-        self.train_S = self.get_data_loader(train_S)
-        self.test_S = self.get_data_loader(test_S)
-
-        self.test_accuracy = 0.0
+        # self.train_S = self.get_data_loader(train_S)
+        # self.test_S = self.get_data_loader(test_S)
 
     def make_hidden_layers(self):
-        self.fc1 = torch_nn.Linear(3 * 2, self.hidden_layer_dimensions)
+        self.fc1 = torch_nn.Linear(10 * 20, self.hidden_layer_dimensions)  # FIXME
         self.fc1.to(self.device)
         for i in range(self.hidden_layers):
             tmp = torch_nn.Linear(
@@ -106,7 +104,7 @@ class NeuralNetwork(torch_nn.Module):
         )
 
     def forward(self, input):
-        flatten_image_input = input.view(-1, 3 * 2).to(self.device)
+        flatten_image_input = input.view(-1, 20 * 10).to(self.device)  # FIXME
         output = self.activation_func(
             self.fc1(flatten_image_input.float())
         )  # add hidden layer, with relu activation function
@@ -352,15 +350,31 @@ def objective(trial):
 
 
 if __name__ == '__main__':
-    if cuda.is_available():
-        print("CUDA is available")
-        print(f"GPU name is {cuda.get_device_name(0)}")
-
-    study = optuna.create_study(
-        study_name="PyTorch MNIST",
-        sampler=optuna.samplers.RandomSampler(),
-        direction='maximize',
+    model = NeuralNetwork(
+        pca=200,
+        hidden_layer_dimensions=100,
+        dropout=0,
+        epochs=1,
+        learning_rate=0.1
     )
-    study.optimize(objective, n_jobs=1)
-    # after study -> tensorboard --logdir runs
-    # go to http://localhost:6006
+    model.train_model_pca()
+    model.test_model_pca()
+    print(model.test_accuracy)
+
+    # if cuda.is_available():
+    #     print("CUDA is available")
+    #     print(f"GPU name is {cuda.get_device_name(0)}")
+    #
+    # study = optuna.create_study(
+    #     study_name="PyTorch MNIST",
+    #     # sampler=optuna.samplers.RandomSampler(),
+    #     sampler=optuna.samplers.TPESampler(),
+    #     direction='maximize',
+    # )
+    # study.optimize(
+    #     objective,
+    #     n_jobs=1,
+    #     n_trials=10
+    # )
+    # # after study -> tensorboard --logdir runs
+    # # go to http://localhost:6006
