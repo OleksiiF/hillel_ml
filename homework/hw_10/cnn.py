da6215b5ff2ceee4e31861dcc06a5316e25c5a30
Oleksii <you@example.com>
2022-11-15 19:16:18 +0200

    10 done

--- homework/hw_10/cnn.py
+++ homework/hw_10/cnn.py
@@ -1,7 +1,5 @@
 import itertools
-
 import numpy as np
-import optuna
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,147 +7,108 @@ from matplotlib import pyplot as plt
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from sklearn.metrics import confusion_matrix
-
-from settings import (
-    RANDOM_ROTATION,
-    EPOCHS,
-    LR,
-    KERNEL_SIZE,
-    PADDING,
-    OPTIMIZER,
-    STRIDE
-)
+import torch.nn.functional as F
 
 
 class DataProvider:
-    def __init__(self, random_rotation=10):
-        val_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+    def __init__(self):
+        transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+        self.train_set = datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
+        self.train_loader = torch.utils.data.DataLoader(self.train_set, batch_size=25, shuffle=True)
 
-        train_transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(),
-            transforms.RandomRotation(random_rotation),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
-
-        self.train_dataset = datasets.CIFAR10(
-            root="./data",
-            train=True,
-            download=True,
-            transform=train_transform
-        )
-        self.test_dataset = datasets.CIFAR10(
-            root="./data",
-            train=False,
-            download=True,
-            transform=val_transform
-        )
-        self.train_dataloader = DataLoader(self.train_dataset, batch_size=4, shuffle=True)
-        self.test_dataloader = DataLoader(
-            self.test_dataset,
-            batch_size=4,
-            shuffle=False
-        )
+        self.test_set = datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
+        self.test_loader = torch.utils.data.DataLoader(self.test_set, batch_size=25, shuffle=False)
 
 
 class CNN(nn.Module):
-    def __init__(
-            self,
-            num_classes=10,
-            data_provider=DataProvider(),
-            num_epocs=100,
-            lr=0.1,
-            weight_decay=5e-4,
-            padding=1,
-            kernel_size=3,
-            stride=1,
-            optimizer='Adam'
-    ):
+    def __init__(self, num_epocs=100, data_provider=DataProvider()):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         super().__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(
-                in_channels=64,
-                out_channels=128,
-                kernel_size=kernel_size,
-                padding=padding,
-                stride=stride
-            ),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(
-                in_channels=128,
-                out_channels=256,
-                kernel_size=kernel_size,
-                padding=padding,
-                stride=stride
-            ),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(
-                in_channels=256,
-                out_channels=128,
-                kernel_size=kernel_size,
-                padding=padding,
-                stride=stride
-            ),
-            nn.ReLU(inplace=True),
-        )
-        self.classifier = nn.Linear(in_features=4 * 4 * 128, out_features=num_classes)
-        self.criterion = nn.CrossEntropyLoss()
-        self.optimizer = getattr(optim, optimizer)(
-            self.parameters(),
-            lr=lr,
-            weight_decay=weight_decay
-        )
-
-        self.num_epocs = num_epocs
         self.data_provider = data_provider
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=5)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+
+        self.fc1 = nn.Linear(in_features=16 * 5 * 5, out_features=120)
+        self.fc2 = nn.Linear(in_features=120, out_features=84)
+        self.out = nn.Linear(in_features=84, out_features=10)
+        self.optimizer = optim.Adam(self.parameters(), lr=0.001)
+        self.num_epocs = num_epocs
         self.__all_preds = torch.tensor([]).to(self.device)
+
         self.to(self.device)
 
-    def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+        for name, param in self.state_dict().items():
+            print(name, param.size())
+
+    def forward(self, t):
+        # Layer 1
+        t = t
+        # Layer 2
+        t = self.conv1(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)  # output shape : (6,14,14)
+        # Layer 3
+        t = self.conv2(t)
+        t = F.relu(t)
+        t = F.max_pool2d(t, kernel_size=2, stride=2)  # output shape : (16,5,5)
+        # Layer 4
+        t = t.reshape(-1, 16 * 5 * 5)
+        t = self.fc1(t)
+        t = F.relu(t)  # output shape : (1,120)
+        # Layer 5
+        t = self.fc2(t)
+        t = F.relu(t)  # output shape : (1, 84)
+        # Layer 6/ Output Layer
+        t = self.out(t)  # output shape : (1,10)
+
+        return t
 
     def train_nn(self):
+        total_corrects = []
+        total_losses =[]
         for epoch in range(self.num_epocs):
             total_correct = 0
             total_loss = 0
-            for imgs, labels in self.data_provider.train_dataloader:
+
+            for imgs, labels in self.data_provider.train_loader:
                 imgs, labels = imgs.to(self.device), labels.to(self.device)
+
+                preds = self(imgs)
+                loss = F.cross_entropy(preds, labels)  # Calculate Los
                 self.optimizer.zero_grad()
-                output = self(imgs)
-                loss = self.criterion(output, labels)  # Calculate Loss
                 loss.backward()  # Calculate gradients
                 self.optimizer.step()  # Update weights
 
                 total_loss += loss.item()
-                total_correct += output.argmax(dim=1).eq(labels).sum().item()
+                total_correct += preds.argmax(dim=1).eq(labels).sum().item()
 
             print('epoch:', epoch, "total_correct:", total_correct, "loss:", total_loss)
+            total_corrects.append(total_correct)
+            total_losses.append(total_loss)
+
+        plt.plot(list(range(1, self.num_epocs+1)), total_corrects, label ='Total corrects')
+        plt.plot(list(range(1, self.num_epocs+1)), total_losses, '-.', label ='Total losses')
+        plt.legend(loc="upper left")
+
+        plt.xlabel("Epochs")
+        plt.title('Total corrects and losses')
+
+        plt.savefig('Total corrects and losses.png')
+        plt.show(block=False)
+        plt.pause(5)
+        plt.close()
 
     @torch.no_grad()
     def test_nn(self):
-        for images, labels in self.data_provider.test_dataloader:
+        for images, labels in self.data_provider.test_loader:
             images, labels = images.to(self.device), labels.to(self.device)
             preds = self(images)
             self.__all_preds = torch.cat((self.__all_preds, preds), dim=0)
 
-        actual_labels = torch.Tensor(self.data_provider.test_dataset.targets).to(self.device)
+        actual_labels = torch.Tensor(self.data_provider.test_set.targets).to(self.device)
         preds_correct = self.__all_preds.argmax(dim=1).eq(actual_labels).to(self.device).sum().item()
 
-        self.accuracy = preds_correct / len(self.data_provider.test_dataset)
-
+        self.accuracy = preds_correct / len(self.data_provider.test_set)
 
     def plot_confusion_matrix(
             self,
@@ -159,7 +118,7 @@ class CNN(nn.Module):
             cmap=plt.cm.Blues
     ):
         cm = confusion_matrix(
-            self.data_provider.test_dataset.targets,
+            self.data_provider.test_set.targets,
             self.__all_preds.cpu().argmax(dim=1)
         )
         print(cm)
@@ -193,43 +152,9 @@ class CNN(nn.Module):
         plt.close()
 
 
-def objective(trial):
-    data_provider = DataProvider(**{
-        'random_rotation': trial.suggest_int(**RANDOM_ROTATION),
-    })
-    model = CNN(**{
-        'data_provider': data_provider,
-        'num_epocs': trial.suggest_int(**EPOCHS),
-        'lr': trial.suggest_float(**LR),
-        'kernel_size': trial.suggest_int(**KERNEL_SIZE),
-        'padding': trial.suggest_int(**PADDING),
-        'stride': trial.suggest_int(**STRIDE),
-        'optimizer': trial.suggest_categorical(**OPTIMIZER),
-    })
-    model.train_nn()
-    model.test_nn()
-    print(model.accuracy)
-
-    return model.test_accuracy
-
-
 if __name__ == '__main__':
     model = CNN()
     model.train_nn()
     model.test_nn()
     print(model.accuracy)
     model.plot_confusion_matrix()
-
-    # study = optuna.create_study(
-    #     study_name="PyTorch MNIST",
-    #     # sampler=optuna.samplers.RandomSampler(),
-    #     sampler=optuna.samplers.TPESampler(),
-    #     direction='maximize',
-    # )
-    # study.optimize(
-    #     objective,
-    #     n_jobs=-1,
-    #     n_trials=100
-    # )
-    # # after study -> tensorboard --logdir runs
-    # # go to http://localhost:6006
